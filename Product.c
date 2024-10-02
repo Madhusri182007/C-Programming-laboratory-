d01a34058cb750e02f25d5c90b235423b236d3d3
Madhusri182007 <madhusri182007@gmail.com>
2024-10-02 08:23:02 -0700

    Create Product.c

---
+++ Product.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,Product;
+    printf("Enter two floating point value:");
+    scanf("%f %f",&a,&b);
+    Product=a*b;
+    printf("Product:%.2f",Product);
+    return 0;
+}
