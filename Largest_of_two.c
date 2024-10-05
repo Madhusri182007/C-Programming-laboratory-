9a64e00c9d5901a6d4027980e2f92074fa4f4968
Madhusri182007 <madhusri182007@gmail.com>
2024-10-05 08:43:08 -0700

    Create Largest_of_two.c

---
+++ Largest_of_two.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int A,B;
+    printf("Enter two integer one by one:");
+    scanf("%d",&A);
+    scanf("%d",&B);
+    if(A>B)
+    {
+        printf("%d is Big",A);
+    }
+    else 
+    {
+        printf("%d is Big",B);
+    }
+}
