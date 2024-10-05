9afba727cec17cddb0a5843290ce5443c9c1fe04
Madhusri182007 <madhusri182007@gmail.com>
2024-10-05 08:35:30 -0700

    Create Even_Odd.c

---
+++ Even_Odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int Number;
+    printf("Enter number:");
+    scanf("%d",&Number);
+    if(Number%2==0)
+    {
+        printf("Even Number");
+    }
+    else 
+    {
+        printf("Odd number");
+    }
+}
