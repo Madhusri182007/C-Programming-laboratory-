4b73d181019fc5c41391fb2e934589277e3e6701
Madhusri182007 <madhusri182007@gmail.com>
2024-10-06 02:25:20 -0700

    Create Area of rectangle.c

---
+++ Area of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int Length,Breadth,Area;
+ printf("Enter length:");
+ scanf("%d",&Length);
+ printf("Enter Breadth:");
+ scanf("%d",&Breadth);
+ Area=Length*Breadth;
+ printf("Area of reactangle:%d",Area);
+ return 0;
+}
