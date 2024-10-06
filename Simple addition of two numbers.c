f5864bbffebe9b26a6ed7543e7b0fafe80a9dc65
Madhusri182007 <madhusri182007@gmail.com>
2024-10-06 02:20:29 -0700

    Create Simple addition of two numbers.c

---
+++ Simple addition of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int Num1,Num2,Add;
+ printf("Enter Num1:");
+ scanf("%d",&Num1);
+ printf("Enter Num2:");
+ scanf("%d",&Num2);
+ Add=Num1+Num2;
+ printf("Addition of two numbers:%d",Add);
+ return 0;
+}
