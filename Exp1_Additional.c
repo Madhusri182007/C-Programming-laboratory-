d78a997a2b694480ceada69c653cbd33339c7645
Madhusri182007 <madhusri182007@gmail.com>
2024-10-07 20:11:47 +0530

    Create Exp1_Additional.c

---
+++ Exp1_Additional.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char Character;
+    scanf("%c",&Character);
+    printf("The given character:%c\n",Character);
+    printf("The Lower Case:%c",(int)Character+32);
+}
+
