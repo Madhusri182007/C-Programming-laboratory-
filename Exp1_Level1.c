352ac57783a55b7cc5b1576e4dd03366a9e430bc
Madhusri182007 <madhusri182007@gmail.com>
2024-10-07 20:06:48 +0530

    Create Exp1_Level1.c

---
+++ Exp1_Level1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int Language,Courses,i;
+    scanf("%d",&Language);
+    for(i=1;i<=Language;i++)
+    {
+        Courses=i*2;
+    }
+    printf("%d",Courses);
+    return 0;
+}
+
+
+
