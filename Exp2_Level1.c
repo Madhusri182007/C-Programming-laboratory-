52e90cc1aef421baef0a2355fc1e2ce7d6ba9bbb
Madhusri182007 <madhusri182007@gmail.com>
2024-10-15 19:20:03 +0530

    Create Exp2_Level1.c

---
+++ Exp2_Level1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int Number1,Number2;
+    scanf("%d %d",&Number1,&Number2);
+    if(Number1==Number2)
+    {
+        printf("Both the given numbers are equal");
+    }
+    else
+    {
+        printf("Given numbers are not equal");
+    }
+    return 0;
+}
