5f876399c71612ad45fc744c952ac2abcb85319d
Madhusri182007 <madhusri182007@gmail.com>
2024-10-15 19:21:16 +0530

    Create Exp2_Level2.c

---
+++ Exp2_Level2.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+    char Number;
+    scanf("%c",&Number);
+    switch(Number)
+    {
+        case '1':
+        {
+            printf("ONE");
+            break;
+        }
+        case '2':
+        {
+            printf("TWO");
+            break;
+        }
+        case '3':
+        {
+            printf("THREE");
+            break;
+        }
+        case '4':
+        {
+            printf("FOUR");
+            break;
+        }
+        case '5':
+        {
+            printf("FIVE");
+            break;
+        }
+        case '6':
+        {
+            printf("SIX");
+            break;
+        }
+        case '7':
+        {
+            printf("SEVEN");
+            break;
+        }
+        case '8':
+        {
+            printf("EIGHT");
+            break;
+        }
+        case '9':
+        {
+            printf("NINE");
+            break;
+        }
+        case '0':
+        {
+            printf("Zero");
+            break;
+        }
+        
+    }
+    return 0;
+}
+
+
+
